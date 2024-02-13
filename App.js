aa752fc22ee223877fc77c72d72a9970f77ae78a
marcogael1 <132959641+marcogael1@users.noreply.github.com>
2024-02-12 20:08:47 -0600

    Actualizacion
    1

--- App.js
+++ App.js
@@ -1,69 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView, Dimensions } from 'react-native';
+import { SafeAreaView} from 'react-native';
 import { estilos } from './Componentes/Estilos';
-
-const { width } = Dimensions.get('window');
-const buttonWidth = (width - 40) / 4;
+import { Calculadora} from './Componentes/Calculadora';
 
 const App = () => {
-  const [valor, setValor] = useState('');
-
-  const operaciones = (valorBoton) => {
-    if (valorBoton === 'AC') {
-      setValor('');
-    } else if (valorBoton === '⌫') {
-      setValor(valor.slice(0, -1));
-    } else {
-      setValor(valor + valorBoton);
-    }
-  };
 
   return (
     <SafeAreaView style={estilos.container}>
-      <View style={estilos.display1}>
-        <Text style={estilos.display}>{valor}</Text>
-      </View>
-      <View style={estilos.calculator}>
-        <Cuerpo operaciones={operaciones} />
-      </View>
+      <Calculadora />
     </SafeAreaView>
   );
 };
-
-const Cuerpo = ({ operaciones }) => (
-  <View style={estilos.buttonsContainer}>
-    <Boton texto="AC" onPress={() => operaciones('AC')} special />
-    <Boton texto="⌫" onPress={() => operaciones('⌫')} special />
-    <Boton texto="%" onPress={() => operaciones(' % ')} special />
-    <Boton texto="÷" onPress={() => operaciones(' / ')} special />
-    <Boton texto="7" onPress={() => operaciones('7')} />
-    <Boton texto="8" onPress={() => operaciones('8')} />
-    <Boton texto="9" onPress={() => operaciones('9')} />
-    <Boton texto="X" onPress={() => operaciones(' * ')} special />
-    <Boton texto="4" onPress={() => operaciones('4')} />
-    <Boton texto="5" onPress={() => operaciones('5')} />
-    <Boton texto="6" onPress={() => operaciones('6')} />
-    <Boton texto="-" onPress={() => operaciones(' - ')} special />
-    <Boton texto="1" onPress={() => operaciones('1')} />
-    <Boton texto="2" onPress={() => operaciones('2')} />
-    <Boton texto="3" onPress={() => operaciones('3')} />
-    <Boton texto="+" onPress={() => operaciones(' + ')} special />
-    <Boton texto="0" onPress={() => operaciones('0')} double />
-    <Boton texto="." onPress={() => operaciones('.')} />
-    <Boton texto="=" onPress={() => operaciones('=')} special />
-  </View>
-);
-
-const Boton = ({ texto, onPress, special, double }) => (
-  <TouchableOpacity
-    onPress={onPress}
-    style={[
-      estilos.button,
-      special ? estilos.specialButton : {},
-      double ? { ...estilos.button, width: buttonWidth * 2 + -10 } : {},
-    ]}
-  >
-    <Text style={estilos.buttonText}>{texto}</Text>
-  </TouchableOpacity>
-);
 export default App;
--- Componentes/Estilos.js
+++ Componentes/Estilos.js
@@ -4,7 +4,7 @@ const buttonWidth = (width - 40) / 4;
 
 export const estilos = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 20,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
@@ -13,13 +13,8 @@ export const estilos = StyleSheet.create({
         alignItems: 'center',
         padding: 20
     },
-    display1: {
-        alignItems: 'center',
-        padding: 10,
-        width: '90%'
-    },
     display: {
-        width: '100%',
+        width: '90%', 
         minHeight: 70,
         fontSize: 30,
         backgroundColor: '#fff',
@@ -27,8 +22,11 @@ export const estilos = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 5,
         textAlign: 'right',
-        justifyContent: 'center'
+        padding: 10,
+        paddingRight: 20, 
+        alignSelf: 'center', 
     },
+    
     buttonsContainer: {
         flexDirection: 'row',
         flexWrap: 'wrap',
@@ -37,7 +35,7 @@ export const estilos = StyleSheet.create({
     button: {
         justifyContent: 'center',
         alignItems: 'center',
-        margin: 5,
+        margin: 9,
         width: buttonWidth - 10,
         height: buttonWidth - 10,
         backgroundColor: '#f0f0f0',
